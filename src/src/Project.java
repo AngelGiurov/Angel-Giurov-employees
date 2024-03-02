6d2175c623794ac69c1d624a436a863e3772bf71
AngelGiurov <angelgiurov2000@abv.bg>
2024-03-02 10:07:35 +0200

    add Project Class

---
+++ src/src/Project.java
@@ -0,0 +1,31 @@
+import java.util.Date;
+
+public class Project {
+    private final int employeeId;
+    private final int projectId;
+    private final Date dateFrom;
+    private final Date dateTo;
+
+    public Project(int employeeId, int projectId, Date dateFrom, Date dateTo) {
+        this.employeeId = employeeId;
+        this.projectId = projectId;
+        this.dateFrom = dateFrom;
+        this.dateTo = dateTo;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public int getProjectId() {
+        return projectId;
+    }
+
+    public Date getDateFrom() {
+        return dateFrom;
+    }
+
+    public Date getDateTo() {
+        return dateTo;
+    }
+}
