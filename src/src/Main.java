8bf76e7d3b23fee0931b4232ce2951488e29c08a
AngelGiurov <angelgiurov2000@abv.bg>
2024-03-03 11:06:29 +0200

    Add Main logic to finalize task requirements

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,5 +1,37 @@
-public class Main {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.*;
+
+public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final List<Project> projects = readProjectsFromCSV();
+        final Map<String, Map<String, Long>> pairDurations = Utils.calculatePairDurations(projects);
+
+        Map.Entry<String, Map<String, Long>> longestPair = Utils.findLongestPair(pairDurations);
+        if (longestPair != null) {
+            final String[] employees = longestPair.getKey().split(",");
+            final long maxDuration = longestPair.getValue().values().stream().mapToLong(Long::longValue).max().orElse(0);
+            System.out.println(employees[0] + ", " + employees[1] + ", " + maxDuration);
+        }
+    }
+
+    private static List<Project> readProjectsFromCSV() {
+        final List<Project> projects = new ArrayList<>();
+
+        try (final BufferedReader br = new BufferedReader(new FileReader("input.csv"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                final String[] parts = line.split(", ");
+//                final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                final LocalDate dateTo = parts[3].equals("NULL") ? LocalDate.now() : LocalDate.parse(parts[3]);
+                projects.add(new Project(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]), LocalDate.parse(parts[2]), dateTo));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return projects;
     }
 }
