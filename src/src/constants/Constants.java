e72b43c465ea73fa1c9381a328dd1f98393cf611
AngelGiurov <angelgiurov2000@abv.bg>
2024-03-03 13:14:36 +0200

    Add Constants to refactor code and move Project to folder

---
+++ src/src/constants/Constants.java
@@ -0,0 +1,13 @@
+package constants;
+
+public class Constants {
+    public static final String COMMA = ",";
+    public static final String SPACE_COMMAS = ", ";
+    public static final String UI_NAME = "Pair Durations Viewer";
+    public static final String SELECT_CSV_FILE = "Select CSV File";
+    public static final String CSV_FILE_NAME = "input.csv";
+    public static final String NULL = "NULL";
+    public static final String NO_PROJECTS_FOUND = "No projects found in the file.";
+    public static final String NO_PAIR_FOUND = "No longest pair found.";
+
+}
